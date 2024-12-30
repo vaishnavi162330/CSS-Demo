d25a88ad1290da776b0d56cf14e746968aca0c38
Vaishnavi Nikumbh <vaishnavi.official30@gmail.com>
2024-12-30 07:18:38 -0800

    Add files via upload

---
+++ ese1.java
@@ -0,0 +1,12 @@
+/*Write a for loop that prints the even numbers from 1 to 20.  */
+
+class ese1{
+    public static void main(String args[]){
+
+        for(int i = 1; i <= 20; i++){
+            if(i % 2 == 0){
+            System.out.println(i);
+            }
+        }
+    }
+}
